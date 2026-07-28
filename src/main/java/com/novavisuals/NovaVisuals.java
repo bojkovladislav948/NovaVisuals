b97fd4680cd01b6cc8506544fdb0bd54da140abb
bojkovladislav948 <bojkovladislav948@gmail.com>
2026-07-28 23:53:38 +0300

    Create NovaVisuals class with initialization method

---
+++ src/main/java/com/novavisuals/NovaVisuals.java
@@ -0,0 +1,11 @@
+package com.novavisuals;
+
+public class NovaVisuals {
+
+    public static final String NAME = "NovaVisuals";
+    public static final String VERSION = "1.0.0";
+
+    public static void init() {
+        System.out.println(NAME + " loaded successfully!");
+    }
+}
